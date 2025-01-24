299609d0b54d11f179139062b3e8455811d208bf
Datha Shashank <dathashashank2000@gmail.com>
2025-01-24 18:03:42 +0530

    darktheme changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,12 +119,12 @@ contactForm.addEventListener('submit', sendEmail)
 // window.addEventListener('scroll',scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-// const scrollUp = () =>{
-//   const scrollUp = document.getElementById('scroll-up')
-//   this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-//                       : scrollUp.classList.remove('show-scroll')
-// }
-// window.addEventListener('scroll', scrollUp)
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scroll-up')
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
